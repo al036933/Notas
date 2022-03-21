0d31c86bcd12dc1b9400638afcb6769de5bae152
Cristian García Hernández <al036933@ieslaencanta.com>
2022-03-21 10:40:00 +0100

    Práctica haciendo

--- src/es/cristian/Main.java
+++ src/es/cristian/Main.java
@@ -1,8 +1,18 @@
 package es.cristian;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int numero;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("¿Cúantas notas quieres introducir? ");
+        numero = entrada.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(numero);
+        }
     }
 }
