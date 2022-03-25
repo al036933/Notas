40a3aa4bae6586850fae31358eae32fdd736c583
Cristian García Hernández <al036933@ieslaencanta.com>
2022-03-25 13:29:14 +0100

    Práctica haciendo

--- src/es/cristian/Main.java
+++ src/es/cristian/Main.java
@@ -6,13 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    int numero;
+        int nota;
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("¿Cúantas notas quieres introducir? ");
         numero = entrada.nextInt();
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(numero);
+        for (int i = 0; i < numero; i++) {
+            System.out.print("Nota " + (i + 1) + ": ");
+            nota = entrada.nextInt();
         }
+
+        entrada.close();
     }
 }
